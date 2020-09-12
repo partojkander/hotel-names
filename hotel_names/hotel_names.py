959454d900dc4362597717029a01a5c42abb8d73
Par Tojkander <par.tojkander@gmail.com>
2020-09-12 15:09:03 +0200

    Enable custom adjective and noun

--- hotel_names/hotel_names.py
+++ hotel_names/hotel_names.py
@@ -6,9 +6,11 @@ nouns = ["Beach", "Downs", "Hills", "Lake", "Mountains"]
 suffixes = ["Spa", "Hotel", "Resorts"]
 
 
-def get_hotel_name(suffix=None):
-    hotel_suffix = suffix or choice(suffixes)
-    return f"{choice(adjectives)} {choice(nouns)} {hotel_suffix}"
+def get_hotel_name(adjective=None, noun=None, suffix=None):
+    adjective_ = adjective or choice(adjectives)
+    noun_ = noun or choice(nouns)
+    suffix_ = suffix or choice(suffixes)
+    return f"{adjective_} {noun_} {suffix_}"
 
 
 def main():
--- test_hotel_names.py
+++ test_hotel_names.py
@@ -1,6 +1,6 @@
 import unittest
 
-from hamcrest import assert_that, is_not, ends_with
+from hamcrest import assert_that, is_not, ends_with, starts_with, contains_string
 
 from hotel_names import hotel_names
 
@@ -10,8 +10,18 @@ class TestHotelNames(unittest.TestCase):
         name = hotel_names.get_hotel_name()
         assert_that(name, is_not(None))
 
-    def test_suffix(self):
-        suffix = "and Chocolat"
-        name = hotel_names.get_hotel_name(suffix=suffix)
-        assert_that(name, ends_with(suffix))
+    def test_custom_adjective(self):
+        custom_adjective = "Hideous"
+        name = hotel_names.get_hotel_name(adjective=custom_adjective)
+        assert_that(name, starts_with(custom_adjective))
+
+    def test_custom_noun(self):
+        custom_noun = "Watermelon"
+        name = hotel_names.get_hotel_name(noun=custom_noun)
+        assert_that(name, contains_string(custom_noun))
+
+    def test_custom_suffix(self):
+        custom_suffix = "and Chocolat"
+        name = hotel_names.get_hotel_name(suffix=custom_suffix)
+        assert_that(name, ends_with(custom_suffix))
 
