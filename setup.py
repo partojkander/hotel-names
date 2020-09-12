602cd6cb21cbe05af20c6a33d1600505350dfbc8
Par Tojkander <par.tojkander@gmail.com>
2020-09-12 19:50:22 +0200

    Stop supporting py 3.5 because it doesn't have f-strings

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     test_suite='test_hotel_names',
